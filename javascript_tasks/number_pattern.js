fd11dcc0596e69e282c390dddd7007d595d9138c
Vidyashree Bhat <vidyashre.bhat@v2solutions.com>
2020-01-20 17:46:36 +0530

    Js pattern programs are added

---
+++ javascript_tasks/number_pattern.js
@@ -0,0 +1,24 @@
+var row = require('readline');
+var prompts = row.createInterface(process.stdin, process.stdout);
+prompts.question("How many row you want to print ?", function (row_num) {
+    for (var i = 1; i <= row_num; i++) {
+        var k = 0;
+        var l = 1;
+        var num = i;
+        var str = "";
+        for (var j = 1; j <= row_num - i; j++) {
+            str = str + " ";
+        }
+        for (var j = 1; j <= i; j++) {
+            str = str + (num % 10);
+            num++;
+        }
+        num--;
+        num--;
+        for (var j = 1; j < i; j++) {
+            str = str + (num % 10);
+            num--;
+        }
+        console.log(str);
+    }
+});
---
+++ javascript_tasks/pascal_triangle.js
@@ -0,0 +1,24 @@
+function fact(n) {
+    var l;
+    var result = 1;
+
+    for (l = 1; l <= n; l++)
+        result = result * l;
+    return result;
+}
+var row = require('readline');
+var prompts = row.createInterface(process.stdin, process.stdout);
+prompts.question("How many row you want to print ?", function (row_num) {
+    var num = 1;
+    for (var i = 0; i <= row_num; i++) {
+        var k = 0;
+        var str = "";
+        for (var j = 1; j <= row_num - i; j++) {
+            str = str + " ";
+        }
+        for (var c = 0; c <= i; c++) {
+            str = str + " " + ((fact(i) / (fact(c) * fact(i - c)) + " ") % 10);
+        }
+        console.log(str)
+    }
+});
