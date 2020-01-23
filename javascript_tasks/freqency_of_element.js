45cb76683c7333b99ec2f073e2bba1db3d7ff725
Vidyashree Bhat <vidyashre.bhat@v2solutions.com>
2020-01-23 09:32:29 +0530

    Finding  frequency of elements withou using extra array

--- javascript_tasks/freqency_of_element.js
+++ javascript_tasks/freqency_of_element.js
@@ -1,20 +1,16 @@
 var arr = [10, 3, 5, 12, 5, 3, 9, 1];
 var i, j, count;
-var freq = [];
 var arrSize = arr.length;
 for (i = 0; i < arrSize; i++) {
-    count = 1;
-    for (j = i + 1; j < arrSize; j++) {
-        if (arr[i] === arr[j]) {
+    count = 0;
+    for (j = 0; j < arrSize; j++) {
+        if (j<i && arr[i] === arr[j]) {
+           break;
+        }
+        else if(arr[i] === arr[j]){
             count++;
-            freq[j] = 0;
         }
     }
-    if (freq[i] !== 0) {
-        freq[i] = count;
-    }
-}
-for (i = 0; i < arrSize; i++) {
-    if (freq[i] !== 0)
-        console.log(arr[i], freq[i]);
+    if(count!==0)
+   console.log(arr[i]+" - "+count );
 }
